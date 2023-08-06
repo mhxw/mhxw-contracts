c10e9f7e90a48b3de560214b6bc6da2a8458836d
mhxw <32643286+mhxw@users.noreply.github.com>
2023-08-06 17:37:19 +0800

    add contracts

--- README.md
+++ README.md
@@ -2,17 +2,51 @@
 
 This project demonstrates a Hardhat and Foundry use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
-Try running some of the following tasks:
+## Installation
 
-```shell
+1. Clone the repo
+2. [Install foundry](https://github.com/foundry-rs/foundry)
+
+```
+git clone https://github.com/mhxw/mhxw-contracts
+cd mhxw-contracts
 pnpm install
-
 forge install
+```
 
+## Setup
+
+### .env
+
+Copy `.env` from `.env.example`
+abd fill in all the variables in `.env`
+
+### hardhat.config.ts
+
+Modify `namedAccounts` in `hardhat.config.ts` and add networks if necessary.
+
+## Test
+
+```
+forge test
+forge test -vvv
+```
+
+## Deployment
+
+```
+npx hardhat deploy --network <NETWORK>
+# or
+npx hardhat run --network <NETWORK> scripts/01-deploy-sunshine-nft.ts
+```
+
+## Other
+
+```
 slither .
 ```
 
-# License
+## License
 
 GNU General Public License v3.0 or later
 
---
+++ contracts/core/SunshineNFT.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.18;
+
+import { ERC721 } from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import { Ownable2Step } from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import { Pausable } from "openzeppelin-contracts/contracts/security/Pausable.sol";
+import { ReentrancyGuard } from "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+import { PullPayment } from "openzeppelin-contracts/contracts/security/PullPayment.sol";
+import { Strings } from "openzeppelin-contracts/contracts/utils/Strings.sol";
+import { ISunshineNFT } from "../interfaces/ISunshineNFT.sol";
+
+error MintPriceNotPaid();
+error MaxSupply();
+error WithdrawTransfer();
+
+/**
+ * @title Sunshine NFT
+ * @author mhxw
+ * @notice This contract is used to create a sunshine NFT.
+ */
+contract SunshineNFT is
+    ERC721,
+    Ownable2Step,
+    Pausable,
+    ReentrancyGuard,
+    PullPayment,
+    ISunshineNFT
+{
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.08 ether;
+    uint256 public currentTokenId;
+    string public baseURI;
+
+    constructor(
+        address _owner,
+        string memory _name,
+        string memory _symbol,
+        string memory _newBaseURI
+    ) ERC721(_name, _symbol) {
+        _transferOwnership(_owner);
+        baseURI = _newBaseURI;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 EXTERNAL
+    //////////////////////////////////////////////////////////////*/
+
+    function mintTo(address recipient) external payable nonReentrant {
+        if (msg.value != MINT_PRICE) {
+            revert MintPriceNotPaid();
+        }
+        uint256 newTokenId = ++currentTokenId;
+        if (newTokenId > TOTAL_SUPPLY) {
+            revert MaxSupply();
+        }
+        _safeMint(recipient, newTokenId);
+    }
+
+    /// @inheritdoc ISunshineNFT
+    function pause(bool toPause) external override onlyOwner {
+        if (toPause) {
+            super._pause();
+        } else {
+            super._unpause();
+        }
+    }
+
+    /**
+     * @notice sets the new baseURI.
+     * @param newBaseURI The baseURI.
+     */
+    function setBaseURI(string memory newBaseURI) external onlyOwner {
+        baseURI = newBaseURI;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 PUBLIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Overridden in order to make it an onlyOwner function
+    function withdrawPayments(address payable payee) public override onlyOwner {
+        super.withdrawPayments(payee);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 INTERNAL
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overridden in child contracts.
+     */
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    /**
+     * @notice Transfers the sunshine nft.
+     * @dev It requires the state to be unpaused
+     * @param from The initial owner address.
+     * @param to The recipient address.
+     * @param id The nft id.
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 id
+    ) internal override whenNotPaused {
+        super._transfer(from, to, id);
+    }
+}
